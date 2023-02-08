ec1435d4dab42e265d7e3091654ba963aac33fa4
Claudio <claudiojbf11@gmail.com>
2023-02-08 14:50:58 -0300

    Pojeto antes dos testes

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from .models import Recipe
+
+# from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes
+    })
+
+
+def category(request, category_id):
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id, is_published=True).order_by('-id')
+    # if not recipes:
+    #     raise Http404('Not Found')
+
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category |'
+    })
+
+
+def recipe(request, id):
+    # recipe = Recipe.objects.filter(id=id).first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
+        'is_detail_page': True,
+    })
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'app.recipes',
 ]
 
@@ -106,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
@@ -121,6 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
